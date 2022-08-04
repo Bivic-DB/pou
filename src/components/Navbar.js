617c9e2332455ce7463b318b8e82f4aeacc2d6aa
bigmarie <nikima06@gmail.com>
2022-08-04 15:23:43 -0600

    cambios

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,15 @@ function Navbar() {
       <div className='leftSide'>
         <img src={Logo} />
       </div>
-      <div className='rightSide'>
+      <div className='middle'>
         <Link to="/"> Inicio </Link>
-
+        <Link to="/servicio"> Servicios </Link>
+        <Link to="/contacto"> Contacto </Link>
+      </div>
+      
+      <div className='rightSide'>
+        <Link to='/inicio-sesion'> Iniciar Sesión </Link>
+        <Link to='/registrarse'> Regístrate </Link>
       </div>
     </div>
   )
