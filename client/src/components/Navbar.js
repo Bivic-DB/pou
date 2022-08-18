aa6f330bb571d092e4e7e9ee9ffe46520831cdab
AcielOb <acielpam@gmail.com>
2022-08-17 20:28:35 -0600

    serv2
    22

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,15 +10,9 @@ function Navbar() {
         <img to="/" src={Logo} id='logo1'/>
       </div>
       <div className='middle'>
-
-        <Link to="/"> Inicio </Link>
-        <Link to="/Servicios"> Servicios </Link>
-        <Link to="/contacto"> Contacto </Link>
-
         <Link className='btnNavbar' to="/"> Inicio </Link>
-        <Link className='btnNavbar' to="/servicio"> Servicios </Link>
+        <Link className='btnNavbar' to="/Servicios"> Servicios </Link>
         <Link className='btnNavbar' to="/contacto"> Contacto </Link>
-
       </div>
       
       <div className='rightSide'>
