06751ae807356f468c317eefff071aaa79056bec
AcielOb <acielpam@gmail.com>
2022-08-17 21:20:11 -0600

    serv3

--- client/src/pages/Servicios.js
+++ client/src/pages/Servicios.js
@@ -12,11 +12,11 @@ function Servicios(){
             <th> Biblioteca del colegio</th>
             <th> Biblioteca de la escuela</th>
         </tr>
-        <tr><td> Préstamo de libros en sala y a domicilio.</td> <td>Préstamo de computadoras.</td></tr>
-        <tr><td> Préstamo de material didáctico.</td><td> Préstamo de material didáctico.</td></tr>
-        <tr><td> Préstamo de computadoras.</td><td> Préstamo de libros en sala y a domicilio.</td></tr>
-        <tr><td> Préstamo de libros en sala y a domicilio.</td><td> Préstamo de libros en sala y a domicilio.</td></tr>
-        <tr><td> Préstamo de calculadoras.</td></tr> 
+        <tr><td class="servcolegio"> Préstamo de libros en sala y a domicilio.</td> <td class="servescuela">Préstamo de computadoras.</td></tr>
+        <tr><td class="servcolegio"> Préstamo de material didáctico.</td><td class="servescuela"> Préstamo de material didáctico.</td></tr>
+        <tr><td class="servcolegio"> Préstamo de computadoras.</td><td class="servescuela"> Préstamo de libros en sala y a domicilio.</td></tr>
+        <tr><td class="servcolegio"> Préstamo de libros en sala y a domicilio.</td><td class="servescuela"> Préstamo de libros en sala y a domicilio.</td></tr>
+        <tr><td class="servcolegio"> Préstamo de calculadoras.</td></tr> 
        </table>
 
        <div class='Importante'>
