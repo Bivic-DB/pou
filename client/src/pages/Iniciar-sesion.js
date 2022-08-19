ad314723c4b55e858b1c3b31650117f13de7c20c
AcielOb <acielpam@gmail.com>
2022-08-18 21:47:44 -0600

    Iniciarses

--- client/src/pages/Iniciar-sesion.js
+++ client/src/pages/Iniciar-sesion.js
@@ -6,6 +6,12 @@ function Iniciars(){
     <div>
       <div id='cont'>
            <h1>Iniciar sesión</h1>
+
+           <input type="text" name="correo"> Correo electrónico</input>
+           <input type="text" name="contrasenna"> Contraseña</input>
+           <input type="text" name="confcontrasenna"> Confirmar contraseña</input>
+           <button>Iniciarsesion</button>
+
       </div>
     </div>
   )
