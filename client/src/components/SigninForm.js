9959e9525a4640f5e52e4dc429d0fd8b0c73ef16
Sebastián <sebasquesada0107@gmail.com>
2022-08-25 00:36:52 -0600

    q
    q

--- client/src/components/SigninForm.js
+++ client/src/components/SigninForm.js
@@ -5,19 +5,20 @@ const SigninForm = () => {
 
     const [Emaillog, setEmaillog] = useState("");
     const [Passwordlog, setPasswordlog] = useState("");
-
+    
     const [loginStatus, setloginStatus] = useState("");
 
     Axios.defaults.withCredentials = true;
 
-    const IniciarSes = () => {
+
+    const IniciarSes = () =>{
         Axios.post('http://localhost:3001/Login', {
             // Objeto con las propiedades que queremos enviar
             Email: Emaillog,
-            Password: Passwordlog,
-
+            Password : Passwordlog,
+            
         }).then((response) => {
-            if (response.data.message) {
+            if (response.data.message){
                 setloginStatus(response.data.message);
             }
             else {
@@ -28,47 +29,30 @@ const SigninForm = () => {
 
     return (
         <div className='Form'>
+            <h1>Iniciar Sesión</h1>
 
+            {/* Input Correo */}
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingEmailLogin" placeholder="Example@example.com" name="Email" onChange={
+                    (event) => { 
+                        setEmaillog(event.target.value)
+                }}/>
+                <label for="floatingName">Correo Electronico</label>
+            </div>
+            {/* Input Contraseña */}
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingPassWordLogin" placeholder="Example@example.com" name="Email" onChange={
+                    (event) => { 
+                        setPasswordlog(event.target.value)
+                }}/>
+                <label for="floatingName">Contraseña</label>
+            </div>
+            <div>
+                <button onClick={IniciarSes} className='submit'>Iniciar</button>
+                <h1>{loginStatus}</h1>
+            </div>
 
-            <div class="form-container sign-in-container">
-                <form action="#">
-                    <h1>Sign in</h1>
-                    {/* Input Correo */}
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingEmailLogin" placeholder="Example@example.com" name="Email" onChange={
-                            (event) => {
-                                setEmaillog(event.target.value)
-                            }} />
-                        <label for="floatingName">Correo Electronico</label>
-                    </div>
-                    {/* Input Contraseña */}
-                    <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="floatingPassWordLogin" placeholder="Example@example.com" name="Email" onChange={
-                            (event) => {
-                                setPasswordlog(event.target.value)
-                            }} />
-                        <label for="floatingName">Contraseña</label>
-                    </div>
-                    <div>
-                        <button onClick={IniciarSes} className='submit'>Iniciar</button>
-                        <h1>{loginStatus}</h1>
-                    </div>
-                </form>
-            </div>
-            <div class="overlay-container">
-                <div class="overlay">
-                    <div class="overlay-panel overlay-left">
-                        <h1>Welcome Back!</h1>
-                        <p>To keep connected with us please login with your personal info</p>
-                        <button class="ghost" id="signIn">Sign In</button>
-                    </div>
-                    <div class="overlay-panel overlay-right">
-                        <h1>Hello, Friend!</h1>
-                        <p>Enter your personal details and start journey with us</p>
-                        <button class="ghost" id="signUp">Sign Up</button>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     )
 }
--- client/src/pages/registrarse.js
+++ client/src/pages/registrarse.js
@@ -6,14 +6,21 @@ import '../styles/Registro.css';
 
 
 function registrarse() {
- 
+
 
     return (
         <div className='Body'>
-            <div class="container" id="container">
-            <SignupForm />
-            <SigninForm />
+
+            <div className='login-container right-panel-active' id='login-container'>
+                <div className='form-container sign-up-container'>
+                    <SignupForm />
+                </div>
+                <div className='form-container sign-in-container'>
+                    <SigninForm />
+                </div>
             </div>
+
+            
         </div>
     )
 }
