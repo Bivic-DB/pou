ab72e54601fecab4aae22bd37c76c21ca6755cfc
AcielOb <acielpam@gmail.com>
2022-09-16 23:45:45 -0600

    y

--- client/src/pages/AgregarNoticia.js
+++ client/src/pages/AgregarNoticia.js
@@ -8,21 +8,21 @@ function AgregarNoticia() {
         <div>
           <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
-    <h4 class="offcanvas-title" id="offcanvasExampleLabel">Agregar Noticia</h4>
+    <h3 class="offcanvas-title" id="offcanvasExampleLabel">Agregar Noticia</h3>
     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
   <div className='container'>
                         <form action='' className='form_comentarios2 d-flex  flex-wrap'>
-                            <h2 className='h2AgregarServicios'>Título</h2>
+                            <h4 className='h4AgregarServicios'>Título</h4>
                             <input type={"text"} name="" id='' placeholder='Título'></input>
-                            <h2 className='h2AgregarServicios'>Información</h2>
+                            <h4 className='h4AgregarServicios'>Información</h4>
                             <input type={"text"} name="" id='' placeholder='Información'></input>
-                            <h2 className='h2AgregarServicios'>Imagen en Carrusel</h2>
+                            <h4 className='h4AgregarServicios'>Imagen en Carrusel</h4>
                             <input type={"file"} name="" id='' placeholder='Imagen en Carrusel'></input>
-                            <h2 className='h2AgregarServicios'>Imagenes Extra</h2>
+                            <h4 className='h4AgregarServicios'>Imagenes Extra</h4>
                             <input type={"file"} name="" id='' placeholder='Imagenes Extra'></input>
-                            <h2 className='h2AgregarServicios'>Fecha</h2>
+                            <h4 className='h4AgregarServicios'>Fecha</h4>
                             <input type={"date"} name="" id='' placeholder='Fecha'></input>
                             <button className='btn' type='button'>Agregar</button>
                         </form>
@@ -31,7 +31,7 @@ function AgregarNoticia() {
 </div>
 
 <button class="btn-offcanvas" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-  Button with data-bs-target
+Agregar noticia
 </button>
         </div>
 
