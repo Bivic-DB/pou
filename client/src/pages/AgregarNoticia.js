8811800fab8e1bae8ddefe5b2b1efa56b0e84832
AcielOb <acielpam@gmail.com>
2022-09-16 23:28:29 -0600

    g

--- client/src/pages/AgregarNoticia.js
+++ client/src/pages/AgregarNoticia.js
@@ -6,10 +6,13 @@ function AgregarNoticia() {
     return (
 
         <div>
-            <div className='container'>
-                <div className='row comentarios2 justify-content-center'>
-                    <div className='col-6'>
-                        <h1 className='h1comentarios2'>Agregar Noticia</h1>
+          <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h4 class="offcanvas-title" id="offcanvasExampleLabel">Agregar Noticia</h4>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+  <div className='container'>
                         <form action='' className='form_comentarios2 d-flex  flex-wrap'>
                             <h2 className='h2AgregarServicios'>Título</h2>
                             <input type={"text"} name="" id='' placeholder='Título'></input>
@@ -23,9 +26,13 @@ function AgregarNoticia() {
                             <input type={"date"} name="" id='' placeholder='Fecha'></input>
                             <button className='btn' type='button'>Agregar</button>
                         </form>
-                    </div>
-                </div>
             </div>
+  </div>
+</div>
+
+<button class="btn-offcanvas" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+  Button with data-bs-target
+</button>
         </div>
 
 
