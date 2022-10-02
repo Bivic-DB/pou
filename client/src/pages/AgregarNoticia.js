b9c4b5cd2549b2cb93794d918865e698ed6213b3
AcielOb <acielpam@gmail.com>
2022-10-02 00:12:57 -0600

    g

--- client/src/pages/AgregarNoticia.js
+++ client/src/pages/AgregarNoticia.js
@@ -6,26 +6,66 @@ function AgregarNoticia() {
     return (
 
         <div>
-            <div className='container'>
-                <div className='row comentarios2 justify-content-center'>
-                    <div className='col-6'>
-                        <h1 className='h1comentarios2'>Agregar Noticia</h1>
+          <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h3 class="offcanvas-title" id="offcanvasExampleLabel">Agregar Noticia</h3>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+  <div className='container'>
                         <form action='' className='form_comentarios2 d-flex  flex-wrap'>
-                            <h2 className='h2AgregarServicios'>Título</h2>
+                            <h4 className='h4AgregarServicios'>Título</h4>
                             <input type={"text"} name="" id='' placeholder='Título'></input>
-                            <h2 className='h2AgregarServicios'>Información</h2>
+                            <h4 className='h4AgregarServicios'>Información</h4>
                             <input type={"text"} name="" id='' placeholder='Información'></input>
-                            <h2 className='h2AgregarServicios'>Imagen en Carrusel</h2>
+                            <h4 className='h4AgregarServicios'>Imagen en Carrusel</h4>
                             <input type={"file"} name="" id='' placeholder='Imagen en Carrusel'></input>
-                            <h2 className='h2AgregarServicios'>Imagenes Extra</h2>
+                            <h4 className='h4AgregarServicios'>Imagenes Extra</h4>
                             <input type={"file"} name="" id='' placeholder='Imagenes Extra'></input>
-                            <h2 className='h2AgregarServicios'>Fecha</h2>
+                            <h4 className='h4AgregarServicios'>Fecha</h4>
                             <input type={"date"} name="" id='' placeholder='Fecha'></input>
                             <button className='btn' type='button'>Agregar</button>
                         </form>
-                    </div>
-                </div>
             </div>
+  </div>
+</div>
+
+<div className='container-sm'>
+<span>
+<h1 id='formsh1'> Administrador de noticias</h1>
+<button class="btn-offcanvas" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
++
+</button>
+</span>
+                <br/>
+                <table className='table table-hover'>
+                    <thead>
+                        <tr>
+                            <th scope="col"> Título</th>
+                            <th scope="col"> Información </th>
+                            <th scope="col"> Imagen </th>
+                            <th scope="col"> Imagenes Extra </th>
+                            <th scope="col"> Fecha </th>
+                            <th scope="col"> Modificar </th>
+                            <th scope="col"> Eliminar </th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td><a className='btn btn-outline-primary' data-bs-toggle="offcanvas" data-bs-target="#offcanvasPlantilla" aria-controls='offcanvasPlantilla' role="button">Modificar</a></td>
+                                    <td><button className='btn btn-danger'>Eliminar</button></td>
+                                </tr>
+                           
+                    </tbody>
+                </table>
+                </div>
         </div>
 
 
--- client/src/pages/AgregarSabiasQue.js
+++ client/src/pages/AgregarSabiasQue.js
@@ -6,29 +6,68 @@ function AgregarSabiasQue() {
     return (
 
         <div>
-            <div className='container'>
-                <div className='row comentarios2 justify-content-center'>
-                    <div className='col-6'>
-                        <h1 className='h1comentarios2'>Agregar Sabías Qué</h1>
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h3 class="offcanvas-title" id="offcanvasExampleLabel">Agregar Sabías Qué</h3>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+  <div className='container'>
                         <form action='' className='form_comentarios2 d-flex  flex-wrap'>
-                            <h2 className='h2AgregarServicios'>Nombre</h2>
+                            <h4 className='h4AgregarServicios'>Nombre</h4>
                             <input type={"text"} name="" id='' placeholder='Nombre'></input>
-                            <h2 className='h2AgregarServicios'>Imagen</h2>
+                            <h4 className='h4AgregarServicios'>Imagen</h4>
                             <input type={"file"} name="" id='' placeholder='# de Servicio'></input>
-                            <h2 className='h2AgregarServicios'>Información</h2>
+                            <h4 className='h4AgregarServicios'>Información</h4>
                             <input type={"text"} name="" id='' placeholder='Información'></input>
-                            <h2 className='h2AgregarServicios'>Link</h2>
+                            <h4 className='h4AgregarServicios'>Link</h4>
                             <input type={"text"} name="" id='' placeholder='Link'></input>
                             <button className='btn' type='button'>Agregar</button>
                         </form>
-                    </div>
-                </div>
             </div>
+  </div>
+</div>
+
+
+<div className='container-sm'>
+<span>
+<h1 id='formsh1'> Administrador de Sabias Qué</h1>
+<button class="btn-offcanvas" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
++
+</button>
+</span>
+                <br/>
+                <table className='table table-hover'>
+                    <thead>
+                        <tr>
+                            <th scope="col"> Nombre</th>
+                            <th scope="col"> Imagen </th>
+                            <th scope="col"> Información </th>
+                            <th scope="col"> Link </th>
+                            <th scope="col"> Modificar </th>
+                            <th scope="col"> Eliminar </th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td><a className='btn btn-outline-primary' data-bs-toggle="offcanvas" data-bs-target="#offcanvasPlantilla" aria-controls='offcanvasPlantilla' role="button">Modificar</a></td>
+                                    <td><button className='btn btn-danger'>Eliminar</button></td>
+                                </tr>
+                           
+                    </tbody>
+                </table>
+                
         </div>
+        </div>
+ 
 
-
-
-    )
+ )
 
 }
 
