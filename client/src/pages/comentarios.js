28e66856aac2d4562f358d993f289a6ea04ce0bb
bigmarie <100313645+bigmarie@users.noreply.github.com>
2022-11-01 00:37:27 -0600

    Com

---
+++ client/src/pages/comentarios.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Administrar.css';
+
+function Comentarios(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default Administrar
